886c91126a9be43ecee0cf72fd5cb0c3db85b30b
Justin Gruenberg <justin.gruenberg1@gmail.com>
2023-05-14 14:19:49 +0200

    basic login functionality

--- app/src/main/java/com/justingruenberg/beatyourneat/LoginActivity.java
+++ app/src/main/java/com/justingruenberg/beatyourneat/LoginActivity.java
@@ -2,15 +2,65 @@ package com.justingruenberg.beatyourneat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.justingruenberg.beatyourneat.Model.DBHelper.UserModelDBHelper;
+import com.justingruenberg.beatyourneat.Model.UserModel;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText et_loginUser;
+    EditText et_loginPassword;
+    Button bt_loginLogin;
+    Button bt_loginRegister;
+    UserModelDBHelper userTable;
+    UserModel user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        
+        et_loginUser = findViewById(R.id.et_loginUser);
+        et_loginPassword = findViewById(R.id.et_loginPassword);
+        bt_loginLogin = findViewById(R.id.bt_loginLogin);
+        bt_loginRegister = findViewById(R.id.bt_loginRegister);
+
+        bt_loginLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userTable = new UserModelDBHelper(LoginActivity.this);
+                String username = et_loginUser.getText().toString();
+                String password = et_loginPassword.getText().toString();
+
+                if(userTable.checkUserExists(username)){
+                    user = userTable.getUser(username);
+                    if(password.equals(user.getPassword())){
+                        // Mainpage
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "wrong password for " + username, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, username + " does not exist", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bt_loginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+
     }
 }
