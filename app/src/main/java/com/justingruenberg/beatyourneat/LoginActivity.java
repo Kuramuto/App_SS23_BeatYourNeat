2876bcdf60d56d6bd142209c4d54873dcdc57ff9
Justin Gruenberg <justin.gruenberg1@gmail.com>
2023-05-23 16:24:09 +0200

    code clean up in login and register

--- app/src/main/java/com/justingruenberg/beatyourneat/LoginActivity.java
+++ app/src/main/java/com/justingruenberg/beatyourneat/LoginActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 import com.justingruenberg.beatyourneat.Model.DBHelper.UserModelDBHelper;
 import com.justingruenberg.beatyourneat.Model.UserModel;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText et_loginUser;
     EditText et_loginPassword;
@@ -32,38 +32,35 @@ public class LoginActivity extends AppCompatActivity {
         et_loginPassword = findViewById(R.id.et_loginPassword);
         bt_loginLogin = findViewById(R.id.bt_loginLogin);
         bt_loginRegister = findViewById(R.id.bt_loginRegister);
+        bt_loginLogin.setOnClickListener(this);
+        bt_loginRegister.setOnClickListener(this);
 
-        bt_loginLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userTable = new UserModelDBHelper(LoginActivity.this);
-                String username = et_loginUser.getText().toString();
-                String password = et_loginPassword.getText().toString();
+    }
 
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "login with your username and password", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (userTable.checkUserExists(username)) {
-                        user = userTable.getUser(username);
-                        if (password.equals(user.getPassword())) {
-                            // Mainpage
-                        } else {
-                            Toast.makeText(LoginActivity.this, "wrong password for " + username, Toast.LENGTH_SHORT).show();
-                        }
+    @Override
+    public void onClick(View view) {
+
+        if (view.equals(bt_loginLogin)) {
+            userTable = new UserModelDBHelper(LoginActivity.this);
+            String username = et_loginUser.getText().toString();
+            String password = et_loginPassword.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "login with your username and password", Toast.LENGTH_SHORT).show();
+            } else {
+                if (userTable.checkUserExists(username)) {
+                    user = userTable.getUser(username);
+                    if (password.equals(user.getPassword())) {
+                        // Mainpage
                     } else {
-                        Toast.makeText(LoginActivity.this, username + " does not exist", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginActivity.this, "wrong password for " + username, Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(LoginActivity.this, username + " does not exist", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
-
-        bt_loginRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
-
-
+        } else if (view.equals(bt_loginRegister)) {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        }
     }
 }
--- app/src/main/java/com/justingruenberg/beatyourneat/RegisterActivity.java
+++ app/src/main/java/com/justingruenberg/beatyourneat/RegisterActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 import com.justingruenberg.beatyourneat.Model.DBHelper.UserModelDBHelper;
 import com.justingruenberg.beatyourneat.Model.UserModel;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText et_registerUser;
     EditText et_registerPassword;
@@ -33,44 +33,35 @@ public class RegisterActivity extends AppCompatActivity {
         bt_registerRegister = findViewById(R.id.bt_registerRegister);
         bt_registerCancel = findViewById(R.id.bt_registerCancel);
 
-        bt_registerRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userTable = new UserModelDBHelper(RegisterActivity.this);
-                String username = et_registerUser.getText().toString();
-                String password = et_registerPassword.getText().toString();
-                String password2 = et_registerPasswordConfirm.getText().toString();
+        bt_registerRegister.setOnClickListener(this);
+        bt_registerCancel.setOnClickListener(this);
 
-                if (username.isEmpty() || password.isEmpty() || password2.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this, "register with username, password and confirm your password", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (!(userTable.checkUserExists(username))) {
-                        if (password.equals(password2)) {
-                            startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
-                            userTable.addUser(new UserModel(username, password));
-                            Toast.makeText(RegisterActivity.this, "welcome! Your registration was successful", Toast.LENGTH_SHORT).show();
-                            try {
-                                Thread.sleep(2000);
-                            } catch (InterruptedException e) {
-                                throw new RuntimeException(e);
-                            }
-                            finish();
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "passwords must be equal", Toast.LENGTH_SHORT).show();
-                        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.equals(bt_registerRegister)) {
+            userTable = new UserModelDBHelper(RegisterActivity.this);
+            String username = et_registerUser.getText().toString();
+            String password = et_registerPassword.getText().toString();
+            String password2 = et_registerPasswordConfirm.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty() || password2.isEmpty()) {
+                Toast.makeText(RegisterActivity.this, "register with username, password and confirm your password", Toast.LENGTH_SHORT).show();
+            } else {
+                if (!(userTable.checkUserExists(username))) {
+                    if (password.equals(password2)) {
+                        userTable.addUser(new UserModel(username, password));
+                        startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
                     } else {
-                        Toast.makeText(RegisterActivity.this, username + " already exists", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RegisterActivity.this, "passwords must be equal", Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(RegisterActivity.this, username + " already exists", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
-
-        bt_registerCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
+        } else if (view.equals(bt_registerCancel)) {
+            finish();
+        }
     }
 }
