a68dab048c18c6c057075ff5196905ca568d1743
Justin Gruenberg <justin.gruenberg1@gmail.com>
2023-06-01 20:47:48 +0200

    creating weightmodel class

--- app/src/main/java/com/justingruenberg/beatyourneat/Model/WeightModel.java
+++ app/src/main/java/com/justingruenberg/beatyourneat/Model/WeightModel.java
@@ -2,4 +2,37 @@ package com.justingruenberg.beatyourneat.Model;
 
 public class WeightModel {
 
+    private String date;
+    private double weight;
+    private UserModel userModel;
+
+    public WeightModel(String date, double weight, UserModel userModel) {
+        this.date = date;
+        this.weight = weight;
+        this.userModel = userModel;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public UserModel getUserModel() {
+        return userModel;
+    }
+
+    public void setUserModel(UserModel userModel) {
+        this.userModel = userModel;
+    }
 }
