87663f729717bbe96b2f8bf3ea44ef0ac13c7746
Justin Gruenberg <justin.gruenberg1@gmail.com>
2023-06-01 20:04:11 +0200

    creating datedialog class wich contains static methods, to create datepickerdialog

--- app/src/main/java/com/justingruenberg/beatyourneat/Activities/AddingWeightActivity.java
+++ app/src/main/java/com/justingruenberg/beatyourneat/Activities/AddingWeightActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 
+import com.justingruenberg.beatyourneat.Dialogs.DateDialog;
 import com.justingruenberg.beatyourneat.Dialogs.HeightDialog;
 import com.justingruenberg.beatyourneat.Dialogs.WeightDialog;
 import com.justingruenberg.beatyourneat.R;
@@ -44,7 +45,7 @@ public class AddingWeightActivity extends AppCompatActivity implements View.OnCl
         if(view.equals(bt_addingWeightWeight)){
             openWeightDialog();
         }else if(view.equals(bt_addingWeightDate)){
-            openDatePickerDialog();
+            DateDialog.openDatePickerDialog(this, bt_addingWeightDate, date);
         }
     }
 
@@ -59,58 +60,4 @@ public class AddingWeightActivity extends AppCompatActivity implements View.OnCl
         bt_addingWeightWeight.setText(weight + " kg");
     }
 
-    public void openDatePickerDialog() {
-
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, android.R.style.Theme_Holo_Light_Panel, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month + 1;
-                date = dateToString(year, month, day);
-                bt_addingWeightDate.setText(date);
-            }
-        }, year, month, day);
-        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-        datePickerDialog.setButton(DatePickerDialog.BUTTON_POSITIVE, "Apply", datePickerDialog);
-        datePickerDialog.setButton(DatePickerDialog.BUTTON_NEGATIVE, "Cancel", datePickerDialog);
-        datePickerDialog.show();
-    }
-
-
-    private String dateToString(int year, int month, int day) {
-        return getMonthFormat(month) + " " + day + " " + year;
-    }
-
-    private String getMonthFormat(int month) {
-
-        switch (month) {
-            case 2:
-                return "FEB";
-            case 3:
-                return "MAR";
-            case 4:
-                return "APR";
-            case 5:
-                return "MAY";
-            case 6:
-                return "JUN";
-            case 7:
-                return "JUL";
-            case 8:
-                return "AUG";
-            case 9:
-                return "SEP";
-            case 10:
-                return "OCT";
-            case 11:
-                return "NOV";
-            case 12:
-                return "DEC";
-            default:
-                return "JAN";
-        }
-    }
 }
---
+++ app/src/main/java/com/justingruenberg/beatyourneat/Dialogs/DateDialog.java
@@ -0,0 +1,68 @@
+package com.justingruenberg.beatyourneat.Dialogs;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class DateDialog {
+
+
+    public static void openDatePickerDialog(Context context, Button button, String date) {
+
+        final String[] chosenDate = {date};
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context, android.R.style.Theme_Holo_Light_Panel, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                chosenDate[0] = dateToString(year, month, day);
+                button.setText(chosenDate[0]);
+            }
+        }, year, month, day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+        datePickerDialog.setButton(DatePickerDialog.BUTTON_POSITIVE, "Apply", datePickerDialog);
+        datePickerDialog.setButton(DatePickerDialog.BUTTON_NEGATIVE, "Cancel", datePickerDialog);
+        datePickerDialog.show();
+    }
+
+    private static String dateToString(int year, int month, int day) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private static String getMonthFormat(int month) {
+
+        switch (month) {
+            case 2:
+                return "FEB";
+            case 3:
+                return "MAR";
+            case 4:
+                return "APR";
+            case 5:
+                return "MAY";
+            case 6:
+                return "JUN";
+            case 7:
+                return "JUL";
+            case 8:
+                return "AUG";
+            case 9:
+                return "SEP";
+            case 10:
+                return "OCT";
+            case 11:
+                return "NOV";
+            case 12:
+                return "DEC";
+            default:
+                return "JAN";
+        }
+    }
+}
