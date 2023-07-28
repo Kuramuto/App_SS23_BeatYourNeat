8a3918ca369005b60994052d54eefe37a33e0f2f
Justin Gruenberg <justin.gruenberg1@gmail.com>
2023-07-29 01:16:23 +0200

    creating Fragments for navbar

--- app/src/main/java/com/justingruenberg/beatyourneat/Fragments/CalculatorFragment.java
+++ app/src/main/java/com/justingruenberg/beatyourneat/Fragments/CalculatorFragment.java
@@ -12,23 +12,21 @@ import com.justingruenberg.beatyourneat.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link CalculatorFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class CalculatorFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private static final String TAG = "HomeFragment";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+    public CalculatorFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +36,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment CalculatorFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static CalculatorFragment newInstance(String param1, String param2) {
+        CalculatorFragment fragment = new CalculatorFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,6 +61,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_calculator, container, false);
     }
 }
